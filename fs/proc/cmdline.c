2f7bc10c054441d154ff70aa80f18affadb8f984
Frostleaft07 <zx7unknow@gmail.com>
2025-01-09 16:10:00 +0700

    SusFS: update to 01072025
    Drop KSU_SUSFS_SUS_SU feature
    
    - KSU_SUSFS_SUS_SU feature is now dropped due to the maintenance difficulties and conflicts with manual hooks
    - KSU_SUSFS_SUS_SU code is removed from most files except susfs.c, susfs.h and ksu_susfs userspace tool, it is just for the consistency over other branches
    
    Signed-off-by: Frostleaft07 <zx7unknow@gmail.com>

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -11,10 +11,9 @@ extern int susfs_spoof_cmdline_or_bootconfig(struct seq_file *m);
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
 #ifdef KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG
-       if (!susfs_spoof_cmdline_or_bootconfig(m)) {
-               seq_putc(m, '\n');
-               return 0;
-       }
+        if (!susfs_spoof_cmdline_or_bootconfig(m)) {
+                seq_putc(m, '\n');
+                return 0;                                                                      }
 #endif
 	seq_printf(m, "%s\n", new_command_line);
 	return 0;
